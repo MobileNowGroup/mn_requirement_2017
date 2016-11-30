2c878af0403a75cd261a8726545f08ceca2a5503
Galen <galen.yuan@mobilenowgroup.com>
2016-11-30 15:51:38 +0800

    check right UA

--- js/mobilenow.js
+++ js/mobilenow.js
@@ -103,7 +103,7 @@
 
   function is_weixin () {
     var ua = navigator.userAgent.toLowerCase()
-    if (ua.match(/MicroMessenger/i) === 'micromessenger') {
+    if (ua.match(/MicroMessenger/i) == 'micromessenger') {
       return true
     } else {
       return false
